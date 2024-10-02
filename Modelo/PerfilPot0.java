651358e2c8d8722db37037d41a9a179940890b5c
Francisco Javier Arruabarrena Sabroso <145534753+jarasa03@users.noreply.github.com>
2024-10-02 16:28:11 +0200

    Javi Pastor

---
+++ Modelo/PerfilPot0.java
@@ -0,0 +1,27 @@
+package Modelo;
+
+public class PerfilPot {
+
+    private int id;
+    public float[] potencia = new float[10];
+
+    public PerfilPot() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float[] getPotencia() {
+        return potencia;
+    }
+
+    public void setPotencia(float[] potencia) {
+        this.potencia = potencia;
+    }
+
+}
