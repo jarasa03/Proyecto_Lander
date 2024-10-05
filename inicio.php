1154ba335bfc178a374fa5b32e271808f5e86948
gokusent <147432051+gokusent@users.noreply.github.com>
2024-10-05 12:51:30 +0200

    Erlin

---
+++ inicio.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página de inicio - Lander2024</title>
+    <style>
+        body{
+            background-color:slategray;
+        }
+        h1{
+            text-align: center;
+            color: crimson;
+        }
+        ul{
+            text-align: center;
+        }
+        ol{
+            border: 3px solid black;
+            background-color: silver;
+            padding: 6px;
+            margin: 5px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Página de inicio - Lander2024</h1>
+    <ul>
+        <ol><a href="DatosSim.php">Datos de Simulacion</a></ol>
+        <ol><a href="Escenario.php">Escenarios</a></ol>
+        <ol><a href="Lander.php">Módulo Lander</a></ol>
+        <ol><a href="PerfilPot.php">Perfil de Potencia</a></ol>
+        <ol><a href="player.php">Jugadores</a></ol>
+        <ol><a href="puntuacion.php">Puntuaciones</a></ol>
+        <ol><a href="SimEngine.php">Motor de Simulaciones</a></ol>
+        <ol><a href="Simulacion.php">Simulaciones</a></ol>
+    </ul>
+</body>
+</html>
